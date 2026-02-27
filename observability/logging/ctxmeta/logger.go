1ca862ff4655a275542cf86e9a5bfd37d2b2afdb
KAnggara75 <KAnggara75@gmail.com>
2026-02-28 05:56:11 +0700

    logger by domain

--- observability/logging/ctxmeta/logger.go
+++ observability/logging/ctxmeta/logger.go
@@ -22,11 +22,49 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type loggerKey string
+
+const (
+	loggerAppKey   loggerKey = "logger_app"
+	loggerDBKey    loggerKey = "logger_db"
+	loggerHTTPKey  loggerKey = "logger_http"
+	loggerKafkaKey loggerKey = "logger_kafka"
+	loggerWAKey    loggerKey = "logger_wa"
+)
+
 func WithLogger(ctx context.Context, logger *logrus.Entry) context.Context {
 	if ctx == nil || logger == nil {
 		return ctx
 	}
 	return context.WithValue(ctx, LoggerKey, logger)
+}
+
+func WithLoggers(ctx context.Context, app, db, http, kafka, wa *logrus.Entry) context.Context {
+	if ctx == nil {
+		return ctx
+	}
+
+	if app != nil {
+		ctx = context.WithValue(ctx, loggerAppKey, app)
+	}
+
+	if db != nil {
+		ctx = context.WithValue(ctx, loggerDBKey, db)
+	}
+
+	if http != nil {
+		ctx = context.WithValue(ctx, loggerHTTPKey, http)
+	}
+
+	if kafka != nil {
+		ctx = context.WithValue(ctx, loggerKafkaKey, kafka)
+	}
+
+	if wa != nil {
+		ctx = context.WithValue(ctx, loggerWAKey, wa)
+	}
+
+	return ctx
 }
 
 func Logger(ctx context.Context) *logrus.Entry {
@@ -39,4 +77,54 @@ func Logger(ctx context.Context) *logrus.Entry {
 	}
 
 	return nil
+}
+
+func LoggerApp(ctx context.Context) *logrus.Entry {
+	if ctx == nil {
+		return logrus.NewEntry(logrus.StandardLogger())
+	}
+	if v, ok := ctx.Value(loggerAppKey).(*logrus.Entry); ok {
+		return v
+	}
+	return logrus.NewEntry(logrus.StandardLogger())
+}
+
+func LoggerDB(ctx context.Context) *logrus.Entry {
+	if ctx == nil {
+		return LoggerApp(ctx)
+	}
+	if v, ok := ctx.Value(loggerDBKey).(*logrus.Entry); ok {
+		return v
+	}
+	return LoggerApp(ctx)
+}
+
+func LoggerHTTP(ctx context.Context) *logrus.Entry {
+	if ctx == nil {
+		return LoggerApp(ctx)
+	}
+	if v, ok := ctx.Value(loggerHTTPKey).(*logrus.Entry); ok {
+		return v
+	}
+	return LoggerApp(ctx)
+}
+
+func LoggerKafka(ctx context.Context) *logrus.Entry {
+	if ctx == nil {
+		return LoggerApp(ctx)
+	}
+	if v, ok := ctx.Value(loggerKafkaKey).(*logrus.Entry); ok {
+		return v
+	}
+	return LoggerApp(ctx)
+}
+
+func LoggerWA(ctx context.Context) *logrus.Entry {
+	if ctx == nil {
+		return LoggerApp(ctx)
+	}
+	if v, ok := ctx.Value(loggerWAKey).(*logrus.Entry); ok {
+		return v
+	}
+	return LoggerApp(ctx)
 }
